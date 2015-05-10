8f5f771b304e2a0e9c26c3d045575ef85742b831
Lifeng Yen <maxasd049@gmail.com>
2015-05-10 22:15:31 +0800

    rename

--- 1dadd.py
+++ 1dadd.py
@@ -2,9 +2,13 @@ from __future__ import division
 import numpy as np
 import math
 
-##one dimensional FDTD test
-##timeframes\abc 
-
+################################################################################
+##final project for general physics in NTU by 顏立峯 and 吳達懿
+##dim : 1d
+##type : gaussian
+##snap : dynamic
+##others : add source
+################################################################################
 size = 200
 
 ez=np.zeros(size)
@@ -16,18 +20,16 @@ maxtime=1000
 
 for i in range(maxtime):
         hy[size-1]=hy[size-2]
-        
+
         hy[0:size-1]=hy[0:size-1]+(ez[1:size]-ez[0:size-1])/imp0
 
         ez[0]=ez[1]
-        
-        #ez[size-1]=ez[size-2]=0
-        
+
         ez[1:size]=ez[1:size]+(hy[1:size]-hy[0:size-1])*imp0
 
         ez[50]+=math.exp(-(i-30)*(i-30)/100)
 
-        
+
 
         if i%10==0:
                 file=open('add50.%d.txt'%int(i/10),'w')
